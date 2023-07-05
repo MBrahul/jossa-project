3510b5d8e1d28fe07b9f8e8c311a0c69b6e46b75
Haribhajan <haribhajank5@gmail.com>
2023-07-05 13:53:56 +0530

    backend commit

---
+++ app.js
@@ -0,0 +1,130 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const { MongoClient } = require("mongodb");
+
+
+
+
+const app = express()
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+const uri = "mongodb+srv://haribhajank5:j7vbWLuUymNCKLFU@cluster2.kp2snpl.mongodb.net/";
+const client = new MongoClient(uri);
+
+const database = client.db('JOSAA');
+const orcr = database.collection('orcr');
+
+
+let userToken = orcr.find({ Year: 2021, Institute:"Indian Institute of Technology Guwahati","Seat_Type":"OPEN",Round:6,Gender:"Gender-Neutral"},{"Academic_Program_Name":1,"Opening_Rank":1,"Closing_Rank":1,"Institute":1,"Year":1}).toArray();
+
+
+let all = orcr.find({}).toArray();
+
+
+
+
+
+
+app.get("/",function(req,res){
+    all.then(function(ele){
+        var iit = [...new Set(ele.map(document => document.Institute))];
+        res.render("trend",{it:iit});
+    })
+})
+
+app.post('/submit', (req, res) => {
+    var selectedOption = req.body.selectedOption;
+    all.then(function(el){
+        var courseNames = el
+        .filter(function(document) {
+            return document.Institute === selectedOption;
+        })
+        .map(function(document) {
+            return document.Academic_Program_Name;
+        });
+        var distinctCourses = Array.from(new Set(courseNames));
+        res.json(distinctCourses);
+    })
+
+  });
+
+app.post('/sub',(req,res)=>{
+    var selectedOption = req.body.selectedOption2;
+
+    all.then(function(el){
+        var Cat = el
+        .filter(function(document) {
+            return (document.Academic_Program_Name === selectedOption);
+        })
+        .map(function(document) {
+            return document.Seat_Type;
+        });
+        var dCat = Array.from(new Set(Cat));
+
+        res.json(dCat);
+    })
+
+});
+
+app.post('/cat',(req,res)=>{
+    var selectedOption = req.body.selectedOption3;
+
+    all.then(function(el){
+        var Cat = el
+        .filter(function(document) {
+            return (document.Seat_Type === selectedOption);
+        })
+        .map(function(document) {
+            return document.Gender;
+        });
+        var dGender = Array.from(new Set(Cat));
+
+        res.json(dGender);
+    })
+
+});
+
+
+app.post('/backendRoute', (req, res) => {
+    var selectedCollege = req.body.college;
+    var selectedCourse = req.body.course;
+    var selectedCategory = req.body.category;
+    var selectedGender = req.body.gender;
+
+
+    
+    all.then(function(el){
+            // Perform filtering on the array of documents based on the selected options
+    const filteredArray = el.filter(document => {
+        return (
+          document.Institute === selectedCollege &&
+          document.Academic_Program_Name === selectedCourse &&
+          document.Seat_Type === selectedCategory &&
+          document.Gender === selectedGender
+        );
+      });
+    
+      // Extract the year and opening rank arrays from the filtered array
+      var years = filteredArray.map(document => document.Year);
+      var openingRanks = filteredArray.map(document => document.Opening_Rank);
+      var dyears = Array.from(new Set(years));
+      var dopeningranks = Array.from(new Set(openingRanks));
+
+    
+      // Construct the response data with the year and opening rank arrays
+      const responseData = { dyears, dopeningranks };
+    
+      res.json(responseData);
+    })
+
+  });
+
+
+app.listen(3000,function(){
+    console.log("Server is up and running on port 3000");
+})
